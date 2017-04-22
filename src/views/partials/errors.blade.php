90d7e2dbfc543d55a12d3210a0bf2f2dd09e816e
namliw <1MJmhOZcbiuY>
2017-04-22 14:44:37 +1000

    added basic validation

---
+++ src/views/partials/errors.blade.php
@@ -0,0 +1,9 @@
+<div class="alert alert-error">
+    <ul>
+        @foreach($errors->all() as $error)
+            <li>
+                {{$error}}
+            </li>
+        @endforeach
+    </ul>
+</div>
